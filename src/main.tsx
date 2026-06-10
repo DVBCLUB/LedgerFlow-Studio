1cdda1ed2e658e7c7405ea1dd6368b39a5723a44
DVBCLUB <davidbao1704@gmail.com>
2026-06-10 08:17:05 +0700

    Use clean simulation guard UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import CT1GlobalSimulationGuard from './components/CT1GlobalSimulationGuard.tsx';
+import SimulationGuard from './components/SimulationGuard.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <CT1GlobalSimulationGuard />
+    <SimulationGuard />
   </StrictMode>,
 );
