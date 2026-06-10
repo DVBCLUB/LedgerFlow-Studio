9d23752b01287c53bef8b7a24819704cbb60128c
DVBCLUB <davidbao1704@gmail.com>
2026-06-10 08:43:57 +0700

    Mount founder labs dock

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import SimulationGuard from './components/SimulationGuard.tsx';
+import FounderLabsDock from './components/FounderLabsDock.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <SimulationGuard />
+    <FounderLabsDock />
   </StrictMode>,
 );
