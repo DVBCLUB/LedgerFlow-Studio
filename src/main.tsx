b52a1abe7d0df792ac6884fccea83f51f0e191a5
DVBCLUB <davidbao1704@gmail.com>
2026-06-12 18:33:26 +0700

    Mount Secret Exposure Guard bridge

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import ApprovedPrPanel from './components/ApprovedPrPanel.tsx';
 import AIOpsCenterLauncher from './components/AIOpsCenterLauncher.tsx';
 import FastReviewModeLauncher from './components/FastReviewModeLauncher.tsx';
 import FastConnectorModeBridge from './components/FastConnectorModeBridge.tsx';
+import SecretExposureGuardBridge from './components/SecretExposureGuardBridge.tsx';
 import './index.css';
 
 function ReviewOverlay() {
@@ -54,6 +55,7 @@ createRoot(document.getElementById('root')!).render(
       <ReviewOverlay />
       <SimulationGuard />
       <FounderLabsDock />
+      <SecretExposureGuardBridge />
       <FastConnectorModeBridge />
       <AIOpsCenterLauncher />
       <FastReviewModeLauncher />
