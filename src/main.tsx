dd583ca67d9df710b3addd6f98583caf76cfa05f
DVBCLUB <davidbao1704@gmail.com>
2026-06-12 15:05:49 +0700

    Wrap app with local desktop login gate

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,19 @@ import AISettingsLauncher from './components/AISettingsLauncher.tsx';
 import IntegrationHubLauncher from './components/IntegrationHubLauncher.tsx';
 import DevHandoffLauncher from './components/DevHandoffLauncher.tsx';
 import GitHubCIDoctorLauncher from './components/GitHubCIDoctorLauncher.tsx';
+import LocalAuthGate from './components/LocalAuthGate.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <SimulationGuard />
-    <FounderLabsDock />
-    <GitHubCIDoctorLauncher />
-    <DevHandoffLauncher />
-    <AISettingsLauncher />
-    <IntegrationHubLauncher />
+    <LocalAuthGate>
+      <App />
+      <SimulationGuard />
+      <FounderLabsDock />
+      <GitHubCIDoctorLauncher />
+      <DevHandoffLauncher />
+      <AISettingsLauncher />
+      <IntegrationHubLauncher />
+    </LocalAuthGate>
   </StrictMode>,
 );
