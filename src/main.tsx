ec49c0d23a3d61a3312845fcd002d3ae8d5e0086
DVBCLUB <davidbao1704@gmail.com>
2026-06-12 15:55:41 +0700

    Mount Review Desk panel

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,19 @@ import IntegrationHubLauncher from './components/IntegrationHubLauncher.tsx';
 import DevHandoffLauncher from './components/DevHandoffLauncher.tsx';
 import GitHubCIDoctorLauncher from './components/GitHubCIDoctorLauncher.tsx';
 import LocalAuthGate from './components/LocalAuthGate.tsx';
+import ApprovedPrPanel from './components/ApprovedPrPanel.tsx';
 import './index.css';
+
+function ReviewOverlay() {
+  const open = window.location.hash === '#/review_desk';
+  return open ? <ApprovedPrPanel /> : null;
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LocalAuthGate>
       <App />
+      <ReviewOverlay />
       <SimulationGuard />
       <FounderLabsDock />
       <GitHubCIDoctorLauncher />
