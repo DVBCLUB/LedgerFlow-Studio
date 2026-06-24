762bba18bb8f892d7ebd4eb9713795ddb4599d76
DVBCLUB <davidbao1704@gmail.com>
2026-06-24 11:57:08 +0700

    Add AI Governance to command center

--- src/modules/ai-hr/AIOperationsCenter.tsx
+++ src/modules/ai-hr/AIOperationsCenter.tsx
@@ -1,3 +1,11 @@
 import AICommandCenterHubPanel from './AICommandCenterHubPanel';
+import AIGovernanceQualityHubPanel from './AIGovernanceQualityHubPanel';
 
-export default AICommandCenterHubPanel;
+export default function AIOperationsCenter() {
+  return (
+    <div className="space-y-6">
+      <AICommandCenterHubPanel />
+      <AIGovernanceQualityHubPanel />
+    </div>
+  );
+}
