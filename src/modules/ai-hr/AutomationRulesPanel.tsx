7696eb962489a57ef90634e511d2ed58e09100ef
DVBCLUB <davidbao1704@gmail.com>
2026-06-24 11:47:49 +0700

    Add automation bridge to automation rules hub

--- src/modules/ai-hr/AutomationRulesPanel.tsx
+++ src/modules/ai-hr/AutomationRulesPanel.tsx
@@ -1,3 +1,11 @@
 import AutomationRobotControlHubPanel from './AutomationRobotControlHubPanel';
+import AutomationBridgeHubPanel from './AutomationBridgeHubPanel';
 
-export default AutomationRobotControlHubPanel;
+export default function AutomationRulesPanel() {
+  return (
+    <div className="space-y-6">
+      <AutomationRobotControlHubPanel />
+      <AutomationBridgeHubPanel />
+    </div>
+  );
+}
